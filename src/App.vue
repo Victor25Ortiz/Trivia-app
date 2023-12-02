835fcd220db3e5f13338497f9553239abee8f3fb
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-01 21:26:39 -0600

    Main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
 <script setup></script>
 
-<template><RouterView />
+<template>
+    <main>
+        <div> class="card">
+        <RouterView />
+        </div>
+    </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen items-center justify-center bg-gradient-to-tr from-indigo-300 to-purple-400;
+
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+
+.score {
+  @apply absolute bottom-6 right-8 text-2xl font-bold uppercase text-slate-700;
+}
+</style>
